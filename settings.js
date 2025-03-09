00214ca3227dac6e9ac37c2550bce017767441bd
Shamikadenuwan <denuwanshamika235@gmail.com>
2025-03-09 09:19:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=lkwBxSyB#XOTIREiBweampAKM4go65bDubs6pSV5RspsXlS7JUWQ",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
